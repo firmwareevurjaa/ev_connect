900d9d43c196fa06622d3580c2b2dd97e4766edd
IT-EVurjaa <it.evurjaa@gmail.com>
2026-05-29 18:54:25 +0530

    port change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const compression = require('compression');
 const helmet = require('helmet');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6001;
 
 // Security middleware
 app.use(helmet({
